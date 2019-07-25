cedfa9a8d0df03dcf74ded9a288528f81b965870
Nick Lee <Nicholas_Lee@uml.edu>
2019-07-25 14:07:50 -0400

    removed travis build and changed color to pink

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Button = styled.button`
-    border: solid 3px pink;
+    border: solid 3px yellow;
     background-color: rgb(47, 101, 175);
     color:white; 
     padding: 8px 16px;
