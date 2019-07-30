c14762e4d5a2a3cfbe66b30863afa3677f22bc17
Nick Lee <Nicholas_Lee@uml.edu>
2019-07-30 15:05:29 -0400

    first commit changed color to red

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Button = styled.button`
-    border: solid 3px yellow;
+    border: solid 3px red;
     background-color: rgb(47, 101, 175);
     color:white; 
     padding: 8px 16px;
