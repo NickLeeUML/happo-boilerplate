9656c5fe3a1ddcf8bb06f95df6486dbc3ed753af
Nick Lee <Nicholas_Lee@uml.edu>
2019-08-21 15:45:01 -0400

    added urls

--- .happo.js
+++ .happo.js
@@ -7,10 +7,16 @@ module.exports = {
   apiSecret: process.env.APISECRET,
 
   targets: {
-    chrome: new RemoteBrowserTarget('chrome', {
+    'chrome-large': new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
     }),
   },
+  pages: [
+    { url: 'http://e93a9051.ngrok.io/Website.UI.Template.v6.happo-url/patterns/components-faq-default.inside.html', title: 'FAQ', variant: "inside"},
+      { url: 'http://e93a9051.ngrok.io/Website.UI.Template.v6.happo-url/patterns/apps-health-provider-search-default.inside.html', title: 'Health Providers', variant: "inside"},
+      { url: 'http://e93a9051.ngrok.io/Website.UI.Template.v6.happo-url/patterns/layout-header-default.default.html', title: 'header', variant: "inside" },
+      { url: 'https://www.uml.edu', title: 'home page'}
+  ],
 
-  type: 'react',
+  // type: 'react',
 };
