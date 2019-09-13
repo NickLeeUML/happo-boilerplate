313fb384f37ad6650394dc84b5c94d79411b1cba
Nick Lee <Nicholas_Lee@uml.edu>
2019-09-13 16:42:18 -0400

    add test puppeteer file

--- src/test/puppeteer.js
+++ src/test/puppeteer.js
@@ -3,8 +3,15 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+  await page.goto('https://stackoverflow.com/', {waitUntil: 'networkidle0'});
+  try {
+    const elemText = await page.$eval("body > header > div > ol.list-reset.grid.gs4 > li > a", elem => elem.innerText)
+    console.log('element innerText:', elemText)
+  } catch(err){
+    console.log(err)
+  }
+  
+  //await page.pdf({path: 'hn.pdf', format: 'A4'});
 
   await browser.close();
 })();
